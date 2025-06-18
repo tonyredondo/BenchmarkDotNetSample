2b2eea1382912ef53dd84dce9eae73c982a78d64
Tony Redondo <tony.redondo@datadoghq.com>
2025-06-18 18:09:40 +0200

    Initial BenchmarkDotNet sample

---
+++ BenchmarkDotNetSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+
+// Execute the benchmarks in the MyBenchmarks class
+BenchmarkRunner.Run<MyBenchmarks>();
+
+
+/// <summary>
+/// Benchmark class for testing performance.
+/// </summary>
+public class MyBenchmarks
+{
+    /// <summary>
+    /// Benchmark method that simulates some work.
+    /// </summary>
+    [Benchmark]
+    public void BenchmarkMethod()
+    {
+        // Simulate some work
+        for (int i = 0; i < 1000; i++)
+        {
+            var temp = Math.Sqrt(i);
+        }
+    }
+}
